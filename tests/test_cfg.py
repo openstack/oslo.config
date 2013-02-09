openstack-archive/oslo-incubator

Implements import_group
Created import_group method in cfg.py
Added testcases

Fixes bug #1107843

Change-Id: I427d4069dacbb6e586687370adbb08b5d50f7b63

tests/unit/test_cfg.py
@@ -1241,6 +1241,23 @@ class ImportOptTestCase(BaseTestCase):
                           'blaa', 'tests.testmods.blaa_opt', group='blaa')
 
 
+class ImportGroupTestCase(BaseTestCase):
+
+    def test_import_group(self):
+        self.assertFalse(hasattr(CONF, 'bar'))
+        CONF.import_group('bar', 'tests.testmods.bar_foo_opt')
+        self.assertTrue(hasattr(CONF, 'bar'))
+        self.assertTrue(hasattr(CONF.bar, 'foo'))
+
+    def test_import_group_import_error(self):
+        self.assertRaises(ImportError, CONF.import_group,
+                          'bar', 'tests.testmods.barrr_foooo_opt')
+
+    def test_import_group_no_such_group(self):
+        self.assertRaises(NoSuchGroupError, CONF.import_group,
+                          'barrr', 'tests.testmods.bar_foo_opt')
+
+
 class RequiredOptsTestCase(BaseTestCase):
 
     def setUp(self):

openstack/oslo.config

tests/test_cfg.py
@@ -1241,6 +1241,23 @@ class ImportOptTestCase(BaseTestCase):
                           'blaa', 'tests.testmods.blaa_opt', group='blaa')
 
 
+class ImportGroupTestCase(BaseTestCase):
+
+    def test_import_group(self):
+        self.assertFalse(hasattr(CONF, 'qux'))
+        CONF.import_group('qux', 'tests.testmods.baz_qux_opt')
+        self.assertTrue(hasattr(CONF, 'qux'))
+        self.assertTrue(hasattr(CONF.qux, 'baz'))
+
+    def test_import_group_import_error(self):
+        self.assertRaises(ImportError, CONF.import_group,
+                          'qux', 'tests.testmods.bazzz_quxxx_opt')
+
+    def test_import_group_no_such_group(self):
+        self.assertRaises(NoSuchGroupError, CONF.import_group,
+                          'quxxx', 'tests.testmods.baz_qux_opt')
+
+
 class RequiredOptsTestCase(BaseTestCase):
 
     def setUp(self):