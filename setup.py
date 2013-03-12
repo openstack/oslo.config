e83c9f311f4abf3dcc728ff617bbb70db417d617
Mark McLoughlin <markmc@redhat.com>
2013-03-12 15:58:46 +0000

    Open 1.2.x development
    The 1.2.x series will be for havana. Its API will be backwards
    compatible with the 1.1.x series.
    
    Change-Id: I041046bb97bf4072f598c91b8a127f4e6bba9f19

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import setuptools
 from magic.openstack.common import setup
 
 package = 'oslo.config'
-version = '1.1.0'
+version = '1.2.0'
 
 requires = setup.parse_requirements()
 depend_links = setup.parse_dependency_links()
