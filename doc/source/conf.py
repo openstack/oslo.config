3497a63ef701d627c8a92777b667b49001b04ef6
Thomas Bechtold <tbechtold@suse.com>
2016-04-11 07:23:40 +0200

    Don't set html_last_updated_fmt without git
    Using a timestamp as value for "html_last_updated_fmt" isn't good
    for the reproducible builds initiative from different downstream
    distributions because the build result always differs due to the
    timestamp in the documentation.
    Also when not setting "html_last_updated_fmt", the value can be set
    via command line when running "sphinx-build -D".
    
    Change-Id: I508499f97cbd075930e9ec1abf6af268881cc1c7

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,7 +3,6 @@
 import os
 import subprocess
 import sys
-import time
 import warnings
 
 sys.path.insert(0, os.path.abspath('../..'))
@@ -65,9 +64,8 @@ try:
     html_last_updated_fmt = subprocess.Popen(
         git_cmd, stdout=subprocess.PIPE).communicate()[0]
 except Exception:
-    warnings.warn('Cannot get last updated time from git repository, '
-                  'using current time.')
-    html_last_updated_fmt = time.ctime()
+    warnings.warn('Cannot get last updated time from git repository. '
+                  'Not setting "html_last_updated_fmt".')
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass
